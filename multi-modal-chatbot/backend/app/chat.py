88abd4370803c3fc0ea7168a973a8ed452552a11
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-10-02 14:06:34 +0500

    Multi Modal Chatbot

--- multi-modal-chatbot/backend/app/chat.py
+++ multi-modal-chatbot/backend/app/chat.py
@@ -1,10 +1,11 @@
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 from langchain.schema.runnable import (
     RunnableLambda,
     RunnableBranch,
 )
 from langchain.schema.output_parser import StrOutputParser
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import os
@@ -12,69 +13,81 @@ import os
 load_dotenv()
 
 # Create models
-openai = ChatOpenAI(model="gpt-4o-mini")
+openai = ChatOpenAI(model="gpt-4")
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
 
 # Function for handling FAQ
 def faq_handling(prompt: str):
+    systemMessage = SystemMessagePromptTemplate.from_template(
+        "You are a knowledgeable assistant trained to handle FAQs about automobiles."
+    )
+    humanMessage = HumanMessagePromptTemplate.from_template("{prompt}")
     faq_handling_template = ChatPromptTemplate.from_messages(
         [
-            ("system", "You are a helpful FAQ assistant that can answer questions about automobiles."),
-            ("human", """You have to give out the details of the product for LinkedIn post about {prompt}.
-        Please follow the following guidelines:
+            systemMessage,
+            humanMessage,
+        ]
+    )
+    return faq_handling_template.format_messages(prompt=prompt)
+
+# Function for content generation
+def content_generation(prompt: str):
+    systemMessage = SystemMessagePromptTemplate.from_template("You are a professional content writer. Write me an interactive LinkedIn post on {prompt}.")
+    humanMessage = HumanMessagePromptTemplate.from_template("""
+    You have to give out the details of the product for LinkedIn post about {prompt}.
+    Please follow the following guidelines:
         - use an eye-catching & interactive hook as an headline of the post in the content of the post,
         - Focus on the latest trends, key players, and significant news related to {prompt}.
         - Develop a detailed content outline that includes an introduction, key points, and a call to action.
         - Include SEO-friendly keywords, relevant hashtags, and data or resources to enhance the post's visibility and impact.
-    """),
-        ]
-    )
-    return faq_handling_template.format_prompt(prompt=prompt)
-
-# Function for content generation
-def content_generation(prompt: str):
+    """)
     content_generation_template = ChatPromptTemplate.from_messages(
         [
-            ("system", "You are a professional content writer. generate me an content for social media post"
-            ),
-            ("human", "{prompt} \n"),
+            systemMessage,
+            humanMessage,
         ]
     )
-    return content_generation_template.format_prompt(prompt=prompt)
+    return content_generation_template.format_messages(prompt=prompt)
 
 # Improved Classification template of the Input
 classification_template = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a knowledgeable assistant trained to classify user prompts."),
+        ("system", """
+        You are a knowledgeable assistant trained to classify user prompts.
+        You are only allow to answer of FAQs about automobiles & generate content for LinkedIn post related prompt.
+        If you don't know the answer, you can say "Ops's Sorry ⚠️⚠️ I am not train on this Prompt Kindly asked me FAQs about automobiles & generate content for LinkedIn post related Question. Have a Good Day"
+        """),
         ("human", "Based on the following prompt, determine whether it pertains to an FAQ about automobiles or content generation. Respond with 'FAQ' or 'Content Generation': \n\n{prompt}\n"),
     ]
 )
 
 # Create branches for these templates
-classification = classification_template | openai | StrOutputParser()
+classification = RunnableLambda(
+    lambda x: {"classification": classification_template | openai | StrOutputParser(), "original_prompt": x}
+)
 
 faq_branch_chain = (
-    RunnableLambda(lambda x: faq_handling(x)) | llm | StrOutputParser()
+    RunnableLambda(lambda x: faq_handling(x["original_prompt"])) | llm | StrOutputParser()
 )
 
 content_branch_chain = (
-    RunnableLambda(lambda x: content_generation(x)) | llm | StrOutputParser()
+    RunnableLambda(lambda x: content_generation(x["original_prompt"])) | llm | StrOutputParser()
 )
 
 # Create the RunnableBranch to route based on classification
-branches: RunnableBranch[str, str] = RunnableBranch(
+branches: RunnableBranch = RunnableBranch(
         (
-            RunnableLambda(lambda x: "FAQ" in x),
+            lambda x: "FAQ" in x["classification"],
             faq_branch_chain,
         ),
-        RunnableLambda(lambda x: "Content" in x) | content_branch_chain,
+        content_branch_chain,
 )
 
 # Combine Classification and Branches
-chain = classification | branches
+chain = RunnableLambda(lambda x: {"prompt": x}) | classification | branches
 
 # Testing the chain with a prompt
-prompt = "Content about Tesla Model S?"
-response = chain.invoke({"prompt": prompt})  # Await the invocation
+prompt = "python code for a calculator"
+response = chain.invoke(prompt)  # Await the invocation if using async
 
 print(response)
